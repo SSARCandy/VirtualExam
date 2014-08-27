1574fde024c7d63962b29c43d4e9e21e28bb954c
SSARCandy <ssarcandy@gmail.com>
2014-08-27 22:30:17 +0800

    [Add] VEServer

---
+++ VEServer/VEServer/OpenExam.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using myexcelcollection;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace VEServer
+{
+    class OpenExam
+    {
+        public MyExcelCollection[] open(string examName)
+        {
+            MyExcelCollection[] mec = null;
+            BinaryFormatter formatter = new BinaryFormatter();
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/題庫/";
+            if (Directory.Exists(path))
+            {
+                Stream stream = new FileStream(path + examName + ".ve", FileMode.Open, FileAccess.Read, FileShare.Read);
+                mec = (MyExcelCollection[])formatter.Deserialize(stream);
+                stream.Close();
+            }
+            return mec;
+        }
+    }
+}
---
+++ VEServer/VEServer/SQLitePath.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VEServer
+{
+    class SQLitePath
+    {
+        public static string path = "C:\\Users\\CYY\\Desktop\\Users.sqlite";
+    }
+}
