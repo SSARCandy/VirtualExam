553bf15a276b9b1fa520d2e6bdf04b8f6a26c74d
ilvmey <ilvmey@yahoo.com.tw>
2014-08-27 22:42:37 +0800

    ???

---
+++ VEServer/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace VEServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblHostIP = new System.Windows.Forms.Label();
+            this.lblHostName = new System.Windows.Forms.Label();
+            this.lblclientComputerName = new System.Windows.Forms.Label();
+            this.lblClientIpAddress = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblOnline = new System.Windows.Forms.Label();
+            this.lblAcc = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.lblSckIndex = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "HostStat";
+            // 
+            // lblHostIP
+            // 
+            this.lblHostIP.AutoSize = true;
+            this.lblHostIP.Location = new System.Drawing.Point(12, 34);
+            this.lblHostIP.Name = "lblHostIP";
+            this.lblHostIP.Size = new System.Drawing.Size(36, 12);
+            this.lblHostIP.TabIndex = 4;
+            this.lblHostIP.Text = "HostIP";
+            // 
+            // lblHostName
+            // 
+            this.lblHostName.AutoSize = true;
+            this.lblHostName.Location = new System.Drawing.Point(12, 9);
+            this.lblHostName.Name = "lblHostName";
+            this.lblHostName.Size = new System.Drawing.Size(53, 12);
+            this.lblHostName.TabIndex = 3;
+            this.lblHostName.Text = "HostName";
+            // 
+            // lblclientComputerName
+            // 
+            this.lblclientComputerName.AutoSize = true;
+            this.lblclientComputerName.Location = new System.Drawing.Point(12, 91);
+            this.lblclientComputerName.Name = "lblclientComputerName";
+            this.lblclientComputerName.Size = new System.Drawing.Size(33, 12);
+            this.lblclientComputerName.TabIndex = 6;
+            this.lblclientComputerName.Text = "label2";
+            // 
+            // lblClientIpAddress
+            // 
+            this.lblClientIpAddress.AutoSize = true;
+            this.lblClientIpAddress.Location = new System.Drawing.Point(12, 118);
+            this.lblClientIpAddress.Name = "lblClientIpAddress";
+            this.lblClientIpAddress.Size = new System.Drawing.Size(33, 12);
+            this.lblClientIpAddress.TabIndex = 7;
+            this.lblClientIpAddress.Text = "label3";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(579, 9);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(124, 244);
+            this.listBox1.TabIndex = 8;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblOnline
+            // 
+            this.lblOnline.AutoSize = true;
+            this.lblOnline.Location = new System.Drawing.Point(12, 173);
+            this.lblOnline.Name = "lblOnline";
+            this.lblOnline.Size = new System.Drawing.Size(53, 12);
+            this.lblOnline.TabIndex = 9;
+            this.lblOnline.Text = "線上人數";
+            // 
+            // lblAcc
+            // 
+            this.lblAcc.AutoSize = true;
+            this.lblAcc.Location = new System.Drawing.Point(12, 151);
+            this.lblAcc.Name = "lblAcc";
+            this.lblAcc.Size = new System.Drawing.Size(53, 12);
+            this.lblAcc.TabIndex = 10;
+            this.lblAcc.Text = "累積人數";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(96, 9);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(477, 244);
+            this.listBox2.TabIndex = 11;
+            // 
+            // lblSckIndex
+            // 
+            this.lblSckIndex.AutoSize = true;
+            this.lblSckIndex.Location = new System.Drawing.Point(12, 202);
+            this.lblSckIndex.Name = "lblSckIndex";
+            this.lblSckIndex.Size = new System.Drawing.Size(46, 12);
+            this.lblSckIndex.TabIndex = 12;
+            this.lblSckIndex.Text = "sckindex";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 261);
+            this.Controls.Add(this.lblSckIndex);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.lblAcc);
+            this.Controls.Add(this.lblOnline);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblClientIpAddress);
+            this.Controls.Add(this.lblclientComputerName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblHostIP);
+            this.Controls.Add(this.lblHostName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblHostIP;
+        private System.Windows.Forms.Label lblHostName;
+        private System.Windows.Forms.Label lblclientComputerName;
+        private System.Windows.Forms.Label lblClientIpAddress;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblOnline;
+        private System.Windows.Forms.Label lblAcc;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label lblSckIndex;
+    }
+}
+
---
+++ VEServer/Form1.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Runtime.Serialization.Formatters.Binary;
+using Microsoft.Office.Interop.Excel;
+using myexcelcollection;
+using veclient;
+using vesocketserver;
+using downloadpath;
+using OnlineClient;
+namespace VEServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        _Application myExcel=null;
+        _Workbook myBook=null;
+        _Worksheet mySheet=null;
+        Range myRange=null;
+        DownloadPath dlPath = new DownloadPath();
+        MyExcelCollection[] question=null;
+        Socket[] sckAccept;
+        VESocket[] SckSs;
+        string LocalIP;
+        int port = 1234;
+        int SckCIndex = 0;
+        VEClient[] vec;
+        OpenExam openExam = new OpenExam();
+
+
+
+        int accumulate = 0;//累積連線人數
+        OC onlineClient = new OC();//在線人數物件
+        int online = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblHostName.Text = Dns.GetHostName();
+            IPAddress[] ipa = Array.FindAll(Dns.GetHostEntry(string.Empty).AddressList, a => a.AddressFamily == AddressFamily.InterNetwork);
+            LocalIP = ipa[0].ToString();
+            lblHostIP.Text = "本機IP=" + ipa[0].ToString();
+            Array.Resize(ref SckSs, 1);
+            Array.Resize(ref sckAccept, 1);
+            Array.Resize(ref vec, 1);
+
+            sckAccept[0] = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sckAccept[0].Bind(new IPEndPoint(IPAddress.Parse(LocalIP), port));
+            sckAccept[0].Listen(5);
+            SckSs[0] = new VESocket();
+            vec[0] = new VEClient();
+            //SckSs[0].setSck();
+            SckSsWaitAccept();
+        }
+
+        private void SckSsWaitAccept()
+        {
+            SckCIndex = SckSs.Length;
+            Array.Resize(ref SckSs, SckCIndex + 1);
+            Array.Resize(ref sckAccept, SckCIndex + 1);
+            Array.Resize(ref vec, SckCIndex + 1);
+            Thread SckSAcceptTd = new Thread(SckSAcceptProc);
+            SckSAcceptTd.Start();
+        }
+
+        private void SckSAcceptProc()
+        {
+            Form.CheckForIllegalCrossThreadCalls = false;
+            int Scki = SckCIndex;
+            SckSs[Scki] = new VESocket();
+            vec[Scki - 1] = new VEClient();
+            sckAccept[Scki] = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                //Socket s = SckSs[Scki].getSck();
+                //s = sckAccept.Accept();
+                //SckSs[Scki].SckAccept(sckAccept);
+                sckAccept[Scki] = sckAccept[0].Accept();
+                accumulate++;
+                lblAcc.Text = Convert.ToString(accumulate);
+                //SckSs[Scki].setSck(s);
+                SckSsWaitAccept();
+                byte[] clientData = new byte[1024];
+                while (true)
+                {
+                   // if (SckSs[Scki].getSck().Connected == true)
+                    if (sckAccept[Scki].Connected == true)
+                    {                      
+                        //SckSs[Scki].getSck().Receive(clientData);
+                        sckAccept[Scki].Receive(clientData);
+                        BinaryFormatter bf = new BinaryFormatter();
+                        MemoryStream stream = new MemoryStream(clientData);
+                        SQLiteDataReader sqlite_datareader;
+                        object obj = bf.Deserialize(stream);
+                        if (obj.GetType() == vec[Scki - 1].GetType())
+                        {
+                            vec[Scki - 1] = (VEClient)obj;
+                            //lblclientComputerName.Text = vec.getComputerName();
+                            lblClientIpAddress.Text = vec[Scki - 1].getIpAddress();
+                            listBox1.Items.Add(vec[Scki - 1].getComputerName());
+                            //讀取SQLite
+                            SQLiteConnection sqlite_conn = new SQLiteConnection();
+                            SQLiteCommand sqlite_cmd = new SQLiteCommand();
+                            
+                            bool isRepeated = true;
+                            #region 檢查重複
+                            
+                            if (vec[Scki - 1].getCommand() == "檢查重複")
+                            {
+                                sqlite_conn = new SQLiteConnection("Data source=" + SQLitePath.path);
+                                sqlite_conn.Open();
+                                sqlite_cmd = sqlite_conn.CreateCommand();
+                                sqlite_cmd.CommandText = "SELECT * FROM UsersData";
+                                sqlite_datareader = sqlite_cmd.ExecuteReader();
+                                while (sqlite_datareader.Read())
+                                {
+                                    if (sqlite_datareader["User"].ToString() == vec[Scki - 1].getUserName())
+                                    {
+                                        vec[Scki - 1].setCommand("重複");
+                                        Serialize(vec[Scki - 1], Scki);
+                                        isRepeated = true;
+                                        break;
+                                    }
+                                    isRepeated = false;
+                                }
+                                if (!isRepeated)
+                                {
+                                    vec[Scki - 1].setCommand("可用");
+                                    Serialize(vec[Scki - 1], Scki);
+                                }
+                                sqlite_datareader.Close();
+                            }
+                            #endregion
+                            #region 註冊
+                            else if(vec[Scki - 1].getCommand()=="註冊")
+                            {
+                                sqlite_conn = new SQLiteConnection("Data source=" + SQLitePath.path);
+                                sqlite_conn.Open();
+                                sqlite_cmd = sqlite_conn.CreateCommand();
+                                sqlite_cmd.CommandText = "SELECT * FROM UsersData";
+                                sqlite_cmd.ExecuteNonQuery();
+                                sqlite_datareader = sqlite_cmd.ExecuteReader();
+                                sqlite_cmd.CommandText = "INSERT INTO UsersData (User,Password,Name) VALUES('" + vec[Scki - 1].getUserName() + "','" + vec[Scki - 1].getPassword() + "','" + vec[Scki - 1].getName() + "')";
+                                sqlite_cmd.ExecuteNonQuery();
+                                sqlite_datareader.Close();
+                            }
+                            #endregion
+                            #region 登入
+                            else
+                            {
+                                sqlite_conn = new SQLiteConnection("Data source=" + SQLitePath.path);
+                                sqlite_conn.Open();
+                                sqlite_cmd = sqlite_conn.CreateCommand();
+                                sqlite_cmd.CommandText = "SELECT * FROM UsersData";
+                                sqlite_datareader = sqlite_cmd.ExecuteReader();
+                                bool isCorrceted = true;
+                                while (sqlite_datareader.Read())
+                                {
+                                    if (sqlite_datareader["User"].ToString() == vec[Scki - 1].getUserName())
+                                    {
+                                        if (sqlite_datareader["Password"].ToString() == vec[Scki - 1].getPassword())
+                                        {
+                                            vec[Scki - 1].setCommand("正確");
+                                            Serialize(vec[Scki - 1], Scki);
+                                            isCorrceted = true;
+                                            break;
+                                        }
+                                    }
+                                    isCorrceted = false;
+                                   
+                                }
+                                if (!isCorrceted)
+                                {
+                                    sckAccept[Scki].Close();
+                                }
+                                sqlite_datareader.Close();
+                                
+                            }
+                            #endregion
+                        }
+                        else if (obj.GetType() == dlPath.GetType())
+                        {
+                            dlPath = (DownloadPath)obj;
+                            sendMyExcelCollection(dlPath.getPath(), Scki);
+                        }
+
+                    }
+                }
+            }
+            catch (Exception e) { MessageBox.Show(e.Message); }
+        }
+        void sendMyExcelCollection(string path,int scki)
+        {
+            question = openExam.open(path);
+            Serialize(question, scki);
+        }
+
+
+        void openExcel(string path)
+        {
+            myExcel = new Microsoft.Office.Interop.Excel.Application();
+            myExcel.Workbooks.Open(path);
+            myExcel.DisplayAlerts = false;
+            myExcel.Visible = false;
+            myBook = myExcel.Workbooks[1];
+            myBook.Activate();
+            mySheet = (_Worksheet)myBook.Worksheets[1];
+            mySheet.Activate();
+
+        }
+        void readExcel()
+        {
+
+            int count = 1;
+            string raws = "A" + count;
+            myRange = mySheet.get_Range(raws);
+            Array.Resize(ref question, 1);
+            while (Convert.ToString(myRange.Value) != null)
+            {
+                raws = "A" + count;
+                myRange = mySheet.get_Range(raws);
+                string q = Convert.ToString(myRange.Value);
+
+                raws = "B" + count;
+                myRange = mySheet.get_Range(raws);
+                string a = Convert.ToString(myRange.Value);
+
+                raws = "C" + count;
+                myRange = mySheet.get_Range(raws);
+                string b = Convert.ToString(myRange.Value);
+
+                raws = "D" + count;
+                myRange = mySheet.get_Range(raws);
+                string c = Convert.ToString(myRange.Value);
+                raws = "E" + count;
+                myRange = mySheet.get_Range(raws);
+                string d = Convert.ToString(myRange.Value);
+
+                raws = "F" + count;
+                myRange = mySheet.get_Range(raws);
+                string ans = Convert.ToString(myRange.Value);
+                question[count - 1] = new MyExcelCollection(q, a, b, c, d, ans);
+                Array.Resize(ref question, question.Length + 1);
+
+                raws = "A" + ++count;
+                myRange = mySheet.get_Range(raws);
+            }
+            Array.Resize(ref question, question.Length - 1);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(myExcel);
+            myBook = null;
+            mySheet = null;
+            myRange = null;
+            myExcel = null;
+            GC.Collect();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            foreach(VESocket s in SckSs)
+            {
+                s.getSck().Close();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            online = 0;
+            onlineClient.SetOnlineClient(0);
+            listBox2.Items.Clear();
+            for(int i=1;i<SckSs.Length-1;i++)
+            {
+                string s = "SocketIndex:" + i + " Connected:" + sckAccept[i].Connected + vec[i - 1].getUserName();
+                listBox2.Items.Add(s);
+                if (sckAccept[i].Connected)
+                    online++;
+            }
+            lblSckIndex.Text = Convert.ToString(SckCIndex);
+           
+            lblOnline.Text = Convert.ToString(online);
+            onlineClient.SetOnlineClient(online);
+            OnlineClientBroadcast();
+        }
+
+        public void Serialize(MyExcelCollection[] m,int scki)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream stream = new MemoryStream();
+            bf.Serialize(stream, m);
+            byte[] bytesSend = new byte[1024];
+            bytesSend = stream.ToArray();
+            sckAccept[scki].Send(bytesSend);
+        }
+        public void Serialize(VEClient vec,int scki)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream stream = new MemoryStream();
+            bf.Serialize(stream, vec);
+            byte[] bytesSend = new byte[1024];
+            bytesSend = stream.ToArray();
+            sckAccept[scki].Send(bytesSend);
+        }
+        public void OnlineClientBroadcast()
+        {
+            for(int i=1;i<sckAccept.Length-1;i++)
+            {
+                if (sckAccept[i].Connected)
+                    Serialize(onlineClient,i);
+            }
+        }
+        public void Serialize(OC oc,int scki)
+        {
+            /*BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream stream = new MemoryStream();
+            bf.Serialize(stream, oc);
+            byte[] bytesSend = new byte[1024];
+            bytesSend = stream.ToArray();
+            sckAccept[scki].Send(bytesSend);*/
+        }
+    }
+}
